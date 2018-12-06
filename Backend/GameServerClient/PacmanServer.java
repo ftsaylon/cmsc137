4b3165c19986d44c357d18f66a855ff9bc74f1c5
JonahCatindig <jbcatindig@up.edu.ph>
2018-12-06 10:32:30 +0800

    Updated setup for server

--- Backend/GameServerClient/PacmanServer.java
+++ Backend/GameServerClient/PacmanServer.java
@@ -8,13 +8,18 @@ import java.awt.*;
 import java.io.*;
 import java.util.*;
 import java.awt.event.*;
-
+import java.net.Socket;
 
 public class PacmanServer implements Runnable{
 	private int player_count;
+	Thread t = new Thread(this);
 
 	public PacmanServer(int player_count){
 		this.player_count = player_count;
+
+		t = new Thread(this);
+
+		t.start();
 	}
 
 	public void run(){
@@ -24,6 +29,12 @@ public class PacmanServer implements Runnable{
 	}
 
 	public static void main(String[] args){
-		
+		if (args.length != 1){
+			System.out.println("Usage: java PacmanServer <number of players>");
+			System.exit(1);
+		}
+
+		new PacmanServer(Integer.parseInt(args[0]));
+
 	}
 }
